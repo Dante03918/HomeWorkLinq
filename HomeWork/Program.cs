e5065788cab7fb19b0c016ccd746cb5e2ca9f686
Dante <dante03918@gmail.com>
2020-05-31 18:22:19 +0200

    Dodaj pliki projektów.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+using Model;
+
+
+
+namespace HomeWork
+{
+    class Program
+    {
+
+        List<Model.Worker> employees = new List<Model.Worker>
+            {
+                new Model.Worker() {name = "Marian", surname = "Kowalski", department = "IT", pass = null},
+                new Model.Worker() {name = "Paweł", surname = "Nowak", department = "HR", pass = null},
+                new Model.Worker() {name = "Piotr", surname = "Mandel", department = "IT", pass = null},
+                new Model.Worker() {name = "Lucjan", surname = "Oruno", department = "LG", pass = null},
+                new Model.Worker() {name = "Rafał", surname = "Lipski", department = "HR", pass = null},
+                new Model.Worker() {name = "Marek", surname = "Mateja", department = "IT", pass = null},
+                new Model.Worker() {name = "Katarzyna", surname = "Sama", department = "LG", pass = null}
+            };
+
+        public void GetWorkersFromItOrHrDepartment()
+        {
+            var workersFromHrOrIt = from worker in employees
+                                    where worker.department == "IT" || worker.department == "HR"
+                                    select worker;
+
+            foreach (var worker in workersFromHrOrIt)
+            {
+                Console.WriteLine("Pracownik działu IT lub HR to: " + $"{worker.surname}");
+            }
+
+        }
+        public void SetRandomPassForAllEmployees()
+        {
+             
+
+            foreach (var worker in employees)
+            {
+                Guid randomPasswd = Guid.NewGuid();
+                worker.pass = randomPasswd.ToString();
+
+                Console.WriteLine("Hasło użytkownika " + $"{worker.surname}" + " to " + $"{worker.pass}");
+            }
+        }
+        public void GetOrderedListWithSurnames()
+        {
+            var orderedEmployees = from worker in employees
+                                   orderby worker.surname ascending
+                                   select worker;
+
+            foreach (var worker in orderedEmployees)
+            {
+                Console.WriteLine($"{worker.surname}");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Program program = new Program();
+            
+
+
+            program.GetOrderedListWithSurnames();
+            program.SetRandomPassForAllEmployees();
+            program.GetWorkersFromItOrHrDepartment();
+
+
+
+            Console.Read();
+
+        }
+
+        
+    }
+
+}
---
+++ Model/Worker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Model
+{
+    public class Worker
+    {
+
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string pass { get; set; }
+        public string department { get; set; }
+        
+        
+        
+        static void Main(string[] args)
+        {
+            int age = 10;
+        }
+    }
+}
